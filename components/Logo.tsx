a9ecdb2a367b3762c1fdb9f70f9b28f9fae25e09
tany903 <work.buffing873@passinbox.com>
2026-08-22 12:59:31 +0800

    minor logo component change

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -7,9 +7,13 @@ export function LogoSmall() {
 
 export function LogoWatermark({ opacity = 0.3 }: { opacity?: number }) {
   return (
-    <div className="fixed pointer-events-none select-none"
-      style={{ opacity, top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '50%', zIndex: 0 }}>
-      <Image src="/logo-big.png" alt="" fill style={{ objectFit: 'contain' }} />
-    </div>
+    <Image
+      src="/logo-big.png"
+      alt=""
+      width={800}
+      height={800}
+      className="fixed pointer-events-none select-none"
+      style={{ opacity, top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '50%', height: 'auto', zIndex: 0 }}
+    />
   )
 }
